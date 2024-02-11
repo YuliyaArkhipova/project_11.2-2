fedd856880b31448d2433ee3a4abc819e8ac8587
YuliyaArkhipova <y.arhipova@bk.ru>
2024-02-11 21:27:04 +0300

    add func_up_first_letter

---
+++ str_func.py
@@ -0,0 +1,11 @@
+
+def func_up(words):
+    """ Функция возвращает все заглавные буквы
+    """
+    return words.upper()
+
+
+def func_up_first_letter(words):
+    """Функция делает заглавной первую букву каждого слова
+    """
+    return words.title()
